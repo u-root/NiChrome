479e21e07d6f3dbfddea632b8d0beeb70cc0fb9f
laconicpneumonic <tonyprolland@gmail.com>
2017-08-17 10:11:52 -0700

    Add Linux 4.12.7 Source Tree

---
+++ linux-4.12.7/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 5 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#5 SMP PREEMPT Wed Aug 16 15:29:21 PDT 2017"
+#define LINUX_COMPILE_BY "arolland"
+#define LINUX_COMPILE_HOST "arolland.mtv.corp.google.com"
+#define LINUX_COMPILER "gcc version 4.8.4 (Ubuntu 4.8.4-2ubuntu1~14.04.3) "
---
+++ linux-4.12.7/include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "4.12.7"
