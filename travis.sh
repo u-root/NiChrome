f06bc148debdf530acfaafc83bc089e01b8b651d
Ronald G. Minnich <rminnich@gmail.com>
2017-09-07 20:31:50 -0700

    fix building and testing
    The travis is now a run of newscript.go
    So no more commits will happen that break the build.
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- travis.sh
+++ travis.sh
@@ -2,13 +2,7 @@
 export GOPATH=/home/travis/gopath
 set -e
 
-# go get is not helpful.
-# go get -d github.com/u-root/u-root
-U=$GOPATH/src/github.com/u-root
-(cd $U && git clone https://github.com/u-root/u-root && cd u-root/bb && go build . && ./bb)
+go run newscript.go -device=/dev/null
 
-rm -rf linux-stable
-git clone --depth 1 -b v4.12.7 git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
-cp /tmp/initramfs.linux_amd64.cpio linux-stable
-(cd linux-stable && cp ../CONFIG .config && make oldconfig &&make -j 8)
+cpio -ivt < /tmp/initramfs.linux_amd64.cpio
 
