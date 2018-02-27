0b22d480fa35f26c897d887245d02a8dc16bbec0
Eric Erfanian <erfanian@users.noreply.github.com>
2018-02-27 10:51:08 -0800

    Test for directory presence before using git.

--- usb/usb.go
+++ usb/usb.go
@@ -239,9 +239,9 @@ func chrome() error {
 }
 
 func kernelGet() error {
-	var updateIfExistsOrClone = []string{"-C", linuxVersion, "pull", "||", "git", "clone", "--depth", "1", "-b", kernelVersion, "git://git.kernel.org/pub/scm/linux/kernel/git/stable/" + linuxVersion + ".git"}
+	var updateIfExistsOrClone = []string{"-d", linuxVersion, "&&", "git", "-C", linuxVersion, "pull", "||", "git", "clone", "--depth", "1", "-b", kernelVersion, "git://git.kernel.org/pub/scm/linux/kernel/git/stable/" + linuxVersion + ".git"}
 	fmt.Printf("-------- updating or fetching the kernel %v\n", updateIfExistsOrClone)
-	cmd := exec.Command("git", updateIfExistsOrClone...)
+	cmd := exec.Command("test", updateIfExistsOrClone...)
 	cmd.Stdin, cmd.Stdout, cmd.Stderr = os.Stdin, os.Stdout, os.Stderr
 	if err := cmd.Run(); err != nil {
 		fmt.Printf("didn't get kernel %v", err)
