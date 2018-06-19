22d26dca54b16bac54cd1f4f2d8a2e0006d48b20
Trevor Farrelly <trevorfarrelly@google.com>
2018-06-19 15:14:21 -0700

    change capitalization of startupcmds to match other global vars
    Signed-off-by: Trevor Farrelly <trevorfarrelly@google.com>

--- cmds/uinit/uinit.go
+++ cmds/uinit/uinit.go
@@ -28,7 +28,7 @@ const (
 )
 
 var (
-	startupCmds   = []string{"sos", "wifi", "upspin_sos"}
+	startupcmds   = []string{"sos", "wifi", "upspin_sos"}
 	cmdline       = make(map[string]string)
 	debug         = func(string, ...interface{}) {}
 	usernamespace = flag.Bool("usernamespace", false, "Set up user namespaces and spawn login")
@@ -394,7 +394,7 @@ func main() {
 		log.Print(err)
 	}
 
-	for _, f := range startupCmds {
+	for _, f := range startupcmds {
 		log.Printf("Run %v", f)
 		go x11(f)
 		// we have to give it a little time until we make it smarter
